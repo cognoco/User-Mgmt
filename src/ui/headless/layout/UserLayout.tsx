d0da919ef7630aa85bde71eb739125d2b48ed84a
crowka <zuzana.kopecna@zwizzly.com>
2025-05-21 14:58:19 +0200

    Add headless layout components

---
+++ src/ui/headless/layout/UserLayout.tsx
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+import { SessionPolicyEnforcer } from '@/ui/styled/session/SessionPolicyEnforcer';
+
+export interface UserLayoutProps {
+  children: (props: { SessionPolicyEnforcer: typeof SessionPolicyEnforcer }) => React.ReactNode;
+}
+
+export function UserLayout({ children }: UserLayoutProps) {
+  return <>{children({ SessionPolicyEnforcer })}</>;
+}
