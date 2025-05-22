380f1e8282dfed04bbb1218d844bfd5c4f7bd118
crowka <zuzana.kopecna@phantix.com>
2025-05-22 15:03:22 +0200

    small things

---
+++ src/adapters/user/interfaces/index.ts
@@ -0,0 +1,9 @@
+/**
+ * User Adapter Interfaces
+ * 
+ * This file exports all user-related adapter interfaces.
+ */
+
+export * from './user-data-provider';
+
+// Add other user-related adapter interfaces here as needed
