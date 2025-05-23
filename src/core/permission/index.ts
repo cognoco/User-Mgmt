42537e21dfc8cebee5caf13366ef9643d4ce1629
crowka <zuzana.kopecna@zwizzly.com>
2025-05-23 15:30:13 +0200

    chore(core): export permission data provider

---
+++ src/core/permission/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Permission Domain Index
+ *
+ * Exports all interfaces, models, and events from the permission domain.
+ */
+
+export * from './interfaces';
+export * from './IPermissionDataProvider';
+export * from './models';
+export * from './events';
