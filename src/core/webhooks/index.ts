24ae086cda34968e3c78fc74369747b3b71c1d17
crowka <zuzana.kopecna@zwizzly.com>
2025-05-25 00:46:06 +0200

    feat(core): add interfaces export and events stub for webhooks

--- src/core/webhooks/index.ts
+++ src/core/webhooks/index.ts
@@ -1,3 +1,4 @@
+export * from './interfaces';
 export * from './IWebhookDataProvider';
-export * from './IWebhookService';
 export * from './models';
+export * from './events';
---
+++ src/core/webhooks/interfaces.ts
@@ -0,0 +1,9 @@
+/**
+ * Webhook Domain Interfaces
+ *
+ * Consolidates the webhook service interface so other parts of the application
+ * have a single import path. Additional interfaces can be added here as the
+ * domain grows.
+ */
+
+export { IWebhookService } from './IWebhookService';
