b256b83b8890910124b1f87d81bd56097b9a30c6
crowka <zuzana.kopecna@zwizzly.com>
2025-05-25 00:46:57 +0200

    feat(adapter): add default organization adapter

---
+++ src/adapters/organization/index.ts
@@ -0,0 +1,4 @@
+export * from '@/core/organization/IOrganizationDataProvider';
+export * from './factory';
+export * from './default-organization-adapter';
+export * from './supabase/supabase-organization.provider';
