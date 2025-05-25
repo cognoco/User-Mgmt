a4020b56a35fabde7c992b547048f55da443c87b
crowka <zuzana.kopecna@zwizzly.com>
2025-05-25 14:37:05 +0200

    fix test compile errors

--- src/ui/styled/profile/__tests__/ProfilePrivacySettings.test.tsx
+++ src/ui/styled/profile/__tests__/ProfilePrivacySettings.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen, waitFor, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import { ProfilePrivacySettings } from '@/ui/styled/profile/ProfilePrivacySettings';
 import { useProfileStore } from '@/lib/stores/profile.store';
 import { usePermission } from '@/hooks/permission/usePermissions';
