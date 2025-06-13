98858da2eddc4553a4dc46f5d3bf2341c50bab5d
crowka <zuzana.kopecna@zwizzly.com>
2025-06-13 15:07:51 +0200

    fix type errors in primitives components

--- src/ui/primitives/tooltip.tsx
+++ src/ui/primitives/tooltip.tsx
@@ -10,9 +10,16 @@ const Tooltip: React.FC<{ children: React.ReactNode }> = ({ children }) => (
   <span className="relative inline-block">{children}</span>
 );
 
-const TooltipTrigger: React.FC<React.ComponentProps<'span'>> = ({ children, ...props }) => (
-  <span {...props}>{children}</span>
-);
+interface TooltipTriggerProps extends React.HTMLAttributes<HTMLSpanElement> {
+  asChild?: boolean;
+}
+
+const TooltipTrigger: React.FC<TooltipTriggerProps> = ({ asChild, children, ...props }) => {
+  if (asChild && React.isValidElement(children)) {
+    return React.cloneElement(children, { ...props });
+  }
+  return <span {...props}>{children}</span>;
+};
 
 const TooltipContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, children, ...props }, ref) => (
